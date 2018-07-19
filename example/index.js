1fff66d062b0a28816261dd4003c98f9cf954d41
Phil Sorber <sorber@apache.org>
2018-07-19 17:51:14 -0400

    Remove 'uri-' as it's now redundant.

--- example/index.js
+++ example/index.js
@@ -40,7 +40,7 @@ var samples = {
   "simple": {
     "exp": 1474243500,
     "iss": "uCDN Inc",
-    "cdniuc": "uri-hash:sha-256;2tderfWPa86Ku7YnzW51YUp7dGUjBS/3SW3ELx4hmWY"
+    "cdniuc": "hash:sha-256;2tderfWPa86Ku7YnzW51YUp7dGUjBS/3SW3ELx4hmWY"
   },
   "complex": {
     "aud": "dCDN LLC",
@@ -52,19 +52,19 @@ var samples = {
     "iss": "uCDN Inc",
     "jti": "5DAafLhZAfhsbe",
     "nbf": 1474243200,
-    "cdniuc": "uri-regex:http://cdni\\.example/foo/bar/[0-9]{3}\\.png"
+    "cdniuc": "regex:http://cdni\\.example/foo/bar/[0-9]{3}\\.png"
   },
   "chained-1": {
     "cdniets": 30,
     "cdnistt": 1,
     "exp": 1474243500,
-    "cdniuc": "uri-regex:http://cdni\\.example/foo/bar/[0-9]{3}\\.ts"
+    "cdniuc": "regex:http://cdni\\.example/foo/bar/[0-9]{3}\\.ts"
   },
   "chained-2": {
     "cdniets": 30,
     "cdnistt": 1,
     "exp": 1474243530,
-    "cdniuc": "uri-regex:http://cdni\\.example/foo/bar/[0-9]{3}\\.ts"
+    "cdniuc": "regex:http://cdni\\.example/foo/bar/[0-9]{3}\\.ts"
   }
 };
 
